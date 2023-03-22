03c134200ae9ff202447c447bf4070d198bd97d6
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2023-03-22 13:25:12 +0300

    Home Screen Header Section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, []);
+
   return (
-    <View>
-      <Text className="text-red-400">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text className="text-red-400">HomeScreen</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
