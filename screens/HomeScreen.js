8792e0c9b71f7fb978ddce3a32318b38fcd170cc
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2023-03-24 15:51:49 +0300

    Home Screen Hero Text

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,9 +13,12 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="text-red-400">HomeScreen</Text>
+    <SafeAreaView className="bg-white flex-1 relative">
+      <View className="flex-row px-6 mt-8 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
+        </View>
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
       </View>
     </SafeAreaView>
   )
