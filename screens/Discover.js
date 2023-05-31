07f26d760186bf1d12161ffd23d096a9d2a96927
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2023-05-31 12:22:57 +0300

    Main Container Top Section

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,9 +1,10 @@
-import { View, Text, SafeAreaView, Image, ScrollView } from 'react-native'
+import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity } from 'react-native'
 import React, {useLayoutEffect, useState} from 'react'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { useNavigation } from '@react-navigation/native';
 import { Avatar, Hotels, Attractions, Restaurants } from '../assets';
 import MenuContainer from '../components/MenuContainer';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Discover = () => {
 
@@ -48,29 +49,41 @@ const Discover = () => {
       </View>
       {/* Menu Container */}
       <ScrollView>
-          <View className="flex-row items-center justify-between px-8 mt-8">
-            <MenuContainer 
-              key={"hotel"}
-              title="Hotels"
-              imageSrc={Hotels}
-              type={type}
-              setType={setType}
-            />
-            <MenuContainer 
-              key={"attractions"}
-              title="Attractions"
-              imageSrc={Attractions}
-              type={type}
-              setType={setType}
-            />
-            <MenuContainer 
-              key={"restaurants"}
-              title="Restaurants"
-              imageSrc={Restaurants}
-              type={type}
-              setType={setType}
-            />
+        <View className="flex-row items-center justify-between px-8 mt-8">
+          <MenuContainer 
+            key={"hotel"}
+            title="Hotels"
+            imageSrc={Hotels}
+            type={type}
+            setType={setType}
+          />
+          <MenuContainer 
+            key={"attractions"}
+            title="Attractions"
+            imageSrc={Attractions}
+            type={type}
+            setType={setType}
+          />
+          <MenuContainer 
+            key={"restaurants"}
+            title="Restaurants"
+            imageSrc={Restaurants}
+            type={type}
+            setType={setType}
+          />
+        </View>
+        <View>
+          <View className="flex-row items-center justify-between px-4 mt-8">
+            <Text className="text-[#2C7379] text-[28px] font-bold">Top Tips</Text>
+            <TouchableOpacity className="flex-row items-center justify-center space-x-2">
+              <Text className="text-[#A0C4C7] text-[20px] font-bold">Explore</Text>
+              <FontAwesome name="long-arrow-right" size={24} color="#A0C4C7" />
+            </TouchableOpacity>
           </View>
+          <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
+
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
