b4e73218c622482126c39dc085a8c9f503156cc6
fatih <fatih.tutar@dijitalvarliklar.com.tr>
2023-06-13 10:58:14 +0300

    Changing the API request dynamically

--- screens/Discover.js
+++ screens/Discover.js
@@ -15,6 +15,10 @@ const Discover = () => {
   const [type, setType] = useState("restaurants");
   const [isLoading, setisLoading] = useState(false);
   const [mainData, setMainData] = useState([]);
+  const [bl_lat, setBl_lat] = useState(null);
+  const [bl_lng, setBl_lng] = useState(null);
+  const [tr_lat, setTr_lat] = useState(null);
+  const [tr_lng, setTr_lng] = useState(null);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -24,13 +28,13 @@ const Discover = () => {
 
   useEffect(() => {
     setisLoading(true);
-    getPlacesData().then(data => {
+    getPlacesData(bl_lat, bl_lng, tr_lat, tr_lng, type).then(data => {
       setMainData(data);
       setInterval(() => {
         setisLoading(false);
       }, 2000);
     })
-  }, []);
+  }, [bl_lat, bl_lng, tr_lat, tr_lng, type]);
 
   return (
     <SafeAreaView className="flex-1 bg-white relative">
@@ -54,6 +58,10 @@ const Discover = () => {
           onPress={(data, details = null) => {
             // 'details' is provided when fetchDetails = true
             console.log(details?.geometry?.viewport);
+            setBl_lat(details?.geometry?.viewport?.southwest?.lat)
+            setBl_lng(details?.geometry?.viewport?.southwest?.lng)
+            setTr_lat(details?.geometry?.viewport?.northeast?.lat)
+            setTr_lng(details?.geometry?.viewport?.northeast?.lng)
           }}
           query={{
             key: 'AIzaSyDhtoyBIjRllV9bkd0JSswG_60V28WAqRE',
@@ -70,7 +78,7 @@ const Discover = () => {
       <ScrollView>
         <View className="flex-row items-center justify-between px-8 mt-8">
           <MenuContainer 
-            key={"hotel"}
+            key={"hotels"}
             title="Hotels"
             imageSrc={Hotels}
             type={type}
